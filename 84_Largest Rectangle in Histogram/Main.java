1629a6298037af2d0c30f2a007f5f4e20f5df43f
Rajat Gupta <90345945+guptraj@users.noreply.github.com>
2023-01-05 14:50:46 +0530

    added

---
+++ 84_Largest Rectangle in Histogram/Main.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void nextLeft(int[] nsol, int[] heights) {
+        Stack<Integer> st = new Stack<>();
+        for (int i = heights.length - 1; i >= 0; i--) {
+            while (st.size() != 0 && heights[st.peek()] > heights[i]) {
+                nsol[st.pop()] = i;
+
+            }
+            st.push(i);
+        }
+    }
+
+    public void nextRight(int[] nsol, int[] heights) {
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < heights.length; i++) {
+            while (st.size() != 0 && heights[st.peek()] > heights[i]) {
+                nsol[st.pop()] = i;
+
+            }
+            st.push(i);
+        }
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] nsor = new int[n];
+        int[] nsol = new int[n];
+        Arrays.fill(nsor, n);
+        Arrays.fill(nsol, -1);
+        nextLeft(nsol, heights);
+        nextRight(nsor, heights);
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int w = nsor[i] - nsol[i] - 1;
+            max = Math.max(max, heights[i] * w);
+        }
+        return max;
+    }
+}
